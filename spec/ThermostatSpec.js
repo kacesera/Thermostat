28fcc307a4072f31c9d7ae60198cf33eaa2eb5b5
KC Thomas <thisiskcthomas@gmail.com>
2020-11-05 16:05:10 +0000

    Temp can be reset, PowerSave can be turned on and off. Added and passed all tests.

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,14 +10,44 @@ describe('Thermostat', () => {
   });
 
   it('can increase temp', () => {
-    thermostat.increaseTemp()
+    thermostat.increaseTemp();
     expect(thermostat.temp).toEqual(21)
   });
 
   it('can decrease temp', () => {
-    thermostat.decreaseTemp()
+    thermostat.decreaseTemp();
     expect(thermostat.temp).toEqual(19)
   });
 
+  it('has a minimum temp of 10 degrees', () => {
+    expect(thermostat.minTemp).toEqual(10);
+  });
 
-})
+  it('has a max temp of 25 degrees, if powerSave is on', () => {
+    expect(thermostat.maxTemp).toEqual(25);
+  });
+
+  it('has a max temp of 32 degrees, if powerSave is off', () => {
+    thermostat.powerSaveOff();
+    expect(thermostat.maxTemp).toEqual(32);
+  });
+  
+  it('has powerSave on as default, but it can be turned off', () => {
+    expect(thermostat.powerSave).toEqual(true);
+    thermostat.powerSaveOff();
+    expect(thermostat.powerSave).toEqual(false);
+  });
+
+  it('can reset temp back to 20 degrees', () => {
+    thermostat.increaseTemp();
+    thermostat.reset();
+    expect(thermostat.temp).toEqual(20);
+    expect(thermostat.powerSave).toEqual(true);
+  });
+
+  it('can turn powerSave on, when it is off', () => {
+    thermostat.powerSaveOff();
+    thermostat.powerSaveOn();
+    expect(thermostat.maxTemp).toEqual(25);
+  });
+});
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,18 +2,32 @@ class Thermostat {
 
   constructor() {
     this.temp = 20;
+    this.minTemp = 10;
+    this.maxTemp = 25;
+    this.powerSave = true;
   };
 
   increaseTemp() {
-
     this.temp++ ;
-
   };
 
   decreaseTemp() {
-
     this.temp-- ;
+  };
 
+  powerSaveOff() {
+    this.powerSave = false;
+    this.maxTemp = 32;
+  };
+
+  powerSaveOn() {
+    this.powerSave = true;
+    this.maxTemp = 25;
+  };
+
+  reset() {
+    this.temp = 20;
+    this.powerSaveOn();
   };
 
 };
